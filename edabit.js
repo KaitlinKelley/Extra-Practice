4a5809cd3dd34eab361f18c684eb9639a5491ca4
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-10-07 10:57:10 -0500

    completed one more exercise in edabit

--- edabit.js
+++ edabit.js
@@ -121,3 +121,15 @@ function length(s) {
     return array.length;
 }
 
+
+
+// Here's an image of four models. Some of the cubes are hidden behind other cubes. Model one consists of one cube. Model two consists of four cubes, and so on...
+//
+// Stack the Boxes
+//
+// Write a function that takes a number n and returns the number of stacked boxes in a model n levels high, visible and invisible.
+
+function stackBoxes(n) {
+    return n**2;
+}
+
