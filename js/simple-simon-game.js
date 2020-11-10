28ac7261a16b644aee13438da263ddc9281f2f84
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-11-10 15:55:52 -0600

    worked on simon game

--- js/simple-simon-game.js
+++ js/simple-simon-game.js
@@ -1,1 +1,23 @@
 "use strict";
+
+var square = $(".square-small");
+var userClicks = 0;
+
+$(square).mousedown(function(){
+    $(this).css("opacity", "100%");
+    userClicks += 1;
+});
+
+$(square).mouseup(function(){
+    $(this).css("opacity", "45%");
+});
+
+var randomNumber = Math.floor(Math.random() * 4) + 1;
+var counterContainer = $(".round-tracker");
+
+function renderRoundTracker(){
+    var html = '<div class="round-tracker-inner">';
+    html += '<div>' + "<p>" + "Round Number: " + randomNumber + '</div>';
+    html += '</div>'
+    return html;
+}
