876989b1df0dace07f29a61f6497d785e7b9df96
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-11-25 20:49:15 -0500

    did one edabit problem

--- edabit.js
+++ edabit.js
@@ -669,3 +669,17 @@ function maxNum(n1,n2) {
         return n2
     }
 }
+
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+//
+//     Examples
+// cubes(3) ➞ 27
+//
+// cubes(5) ➞ 125
+//
+// cubes(10) ➞ 1000
+
+function cubes(a) {
+    return a ** 3;
+}
+
