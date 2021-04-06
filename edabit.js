ba3d6ba08862f440534ff5cdb2097d93d37eeada
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-05 23:35:58 -0500

    worked on hackerrank for a while, stuck on print f problem at the moment

--- edabit.js
+++ edabit.js
@@ -740,3 +740,20 @@ function countBoomerangs(arr) {
 
 }
 
+
+// Create a function that calculates the number of different squares in an n * n square grid. Check the Resources tab.
+//
+//     Examples
+// numberSquares(2) ➞ 5
+//
+// numberSquares(4) ➞ 30
+//
+// numberSquares(5) ➞ 55
+// Notes
+// Input is a positive integer.
+//     Square pyramidal number.
+
+function numberSquares(n) {
+    return n*(n+1)*(2*n+1)/6;
+}
+
