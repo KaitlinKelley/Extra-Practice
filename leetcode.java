124976d8459a945e005e8fb7bcf6033332b455ba
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-09 23:18:27 -0500

    worked on leetcode

--- leetcode.java
+++ leetcode.java
@@ -39,3 +39,44 @@ public int[] twoSum(int[] nums, int target) {
         }
         throw new IllegalArgumentException("No two sum solution");
         }
+===============================================================================
+        Given an integer x, return true if x is palindrome integer.
+
+        An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
+
+
+        Example 1:
+
+        Input: x = 121
+        Output: true
+        Example 2:
+
+        Input: x = -121
+        Output: false
+        Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+        Example 3:
+
+        Input: x = 10
+        Output: false
+        Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+        Example 4:
+
+        Input: x = -101
+        Output: false
+
+        var isPalindrome = function(x) {
+        if (x < 0) {
+        return false;
+        } else if (x === 0) {
+        return true;
+        }
+        let reversed = 0;
+        let y = x;
+        while (y > 0) {
+        const lastDigit = y % 10;
+        reversed = (reversed * 10) + lastDigit;
+        y = Math.floor(y / 10);
+        }
+        return reversed === x;
+        };
