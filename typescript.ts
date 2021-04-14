5277906082a4b36e54c4628f0478eb280fa63855
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-13 23:42:53 -0500

    worked on typescript course

--- typescript.ts
+++ typescript.ts
@@ -143,19 +143,190 @@ function printName(f,l){
 // **Cannot reassign a "let", but you CAN give it a different value**
 // **"var" can be reassigned, as long as it's the same type**
 
-printName("Kaitlin", "Muse");
-
-// **Can use js-style concatenation in ts as well**
-
-var msg : string = "Kaitlin";
-
-
-// **Can also do this!!**
-console.log(`A long message to ${msg} filled with text`);
-
-
-// **ANY valid js can be used in typescript**
-
+// printName("Kaitlin", "Muse");
+//
+// // **Can use js-style concatenation in ts as well**
+//
+// var msg : string = "Kaitlin";
+//
+//
+// // **Can also do this!!**
+// console.log(`A long message to ${msg} filled with text`);
+//
+//
+// // **ANY valid js can be used in typescript**
+//
+// var names : string[] = ["Kaitlin", "John", "Foster"];
+// var counter: number = 0;
+//
+//
+// //immediately invoked function
+// (function(){
+//     for (let name in names){
+//         counter++;
+//     }
+// })();
+//
+// console.log(counter);
+//
+// //Function Expression
+// // var fullName : (first:string, last:string) => string;
+//
+// fullName = function(first:string, last:string){
+//     return first + " " + last;
+// }
+//
+// console.log(fullName("Kaitlin", "Muse"));
+//
+// //different version of the above function!
+// //immediately invoked function arguments
+// (function(first:string, last:string){
+//     console.log(first + " " + last);
+// })("Kaitlin", "Muse");
+//
+// //closures encapsulate data, behaviors, etc.
+// //closures can maintain levels of state
+//
+// //All functions are closures
+//
+// //functions have access to any public variables in the outer scope
+//
+// // function nameFunction(name:string) : void {
+// // 	var n : string = name;
+//
+// // 	function printName(){
+// // 		console.log(n);
+// // 	}
+//
+// // 	printName();
+// // }
+//
+// // nameFunction("Kaitlin");
+//
+//
+//
+//
+//
+//
+// //Inner functions maintain access to the outer scope even AFTER the values are returned!
+// // function nameFunction(name:string){
+// // 	var n : string = name;
+//
+// // 	return function(){
+// // 		console.log(n);
+// // 	}
+// // }
+//
+// // var nameAgain = nameFunction("John");
+// // nameAgain();
+//
+//
+//
+// function lineup(){
+//     var nowBatting : number = 1;
+//
+//     return {
+//         nextBatter(){nowBatting++},
+//         currentBatter(){return nowBatting}
+//     }
+// }
+//
+//
+// let batters = lineup();
+//
+// console.log(batters.currentBatter());
+// batters.nextBatter();
+// console.log(batters.currentBatter());
+// batters.nextBatter();
+// console.log(batters.currentBatter());
+//
+//
+// let pitchers = lineup();
+// console.log(pitchers.currentBatter());
+// //This restarts at 1
+//
+// class Invoice {
+//     companyProfile : string;
+//
+//     constructor(public name, public city, public state){
+//         this.companyProfile = name + ", " + city + ", " + state;
+//     }
+// }
+//
+// var googleInvoice = new Invoice("Google", "Mountain View", "CA");
+// var yahooInvoice = new Invoice("Yahoo", "SF", "CA");
+//
+// console.log(googleInvoice.companyProfile);
+// console.log(yahooInvoice.companyProfile);
+//
+// class Report {
+//     companyProfile: string;
+//
+//     constructor(public name:string){
+//         this.companyProfile = name;
+//     }
+// }
+//
+// class Invoice extends Report {
+//     constructor(public name:string, public total:number){
+//         super(name);
+//     }
+//
+//     printInvoice(){
+//         return this.name + " " + this.total;
+//     }
+// }
+//
+// class BillOfLading extends Report {
+//     constructor(public name:string, public city:string, public state:string){
+//         super(name);
+//     }
+//
+//     printBol(){
+//         return this.name + " " + this.city + " " + this.state;
+//     }
+// }
+//
+// var invoice = new Invoice("Google", 200);
+// var invoiceTwo = new Invoice("Yahoo", 2000);
+// var bol = new BillOfLading("Google", "Scottsdale", "AZ");
+// console.log(invoice.companyProfile);
+// console.log(invoiceTwo.printInvoice());
+// console.log(bol.printBol());
+//
+// var realUser = {
+//     email: "test@test.com",
+//     firstName: "Kaitlin",
+//     lastName: "Muse",
+//     sayHi(){
+//         return "Hey there!";
+//     }
+// };
+//
+// console.log(realUser.sayHi());
+//
+// //class is like a blueprint, interface like a set of rules that objects/classes must follow
+// //doesn't use "implements" keyword??
+//
+// interface User {
+//     email:string;
+//     firstName?:string;
+//     lastName?:string;
+// }
+// //The "?" marks those fields as optional
+//
+// function profile(user:User):string{
+//     return `Welcome, ${user.firstName} ${user.lastName}!`;
+// }
+//
+// //Somehow it knows that this object is a user???
+// var realUser = {
+//     email: "test@test.com",
+//     firstName: "John",
+//     lastName: "Coker"
+// };
+//
+// console.log(profile(realUser));
 
 
 
