1223943a6496c9c951fddbbedcb1176ee806127a
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-16 17:04:00 -0500

    worked more on typescript course

--- typescript.ts
+++ typescript.ts
@@ -391,6 +391,334 @@ console.log(post.title);
 console.log(post.body);
 post.printPost();
 
+//namespaces formerly known as modules
+
+namespace Blog {
+    //export makes these items available outside the namespace??
+    export interface IPost {
+        title:string;
+        body:string;
+    }
+
+    export class Post implements IPost {
+        title:string;
+        body:string;
+
+        constructor(post:IPost){
+            this.title = post.title;
+            this.body = post.body;
+        }
+
+        printPost(){
+            console.log(this.title);
+            console.log(this.body);
+        }
+    }
+}
+
+
+
+
+namespace Content {
+    export interface IPost {
+        title:string;
+        body:string;
+        slug:string;
+        seoKeywords:string;
+    }
+
+    export class Post implements IPost {
+        title:string;
+        body:string;
+        slug:string;
+        seoKeywords:string;
+
+        constructor(post:IPost){
+            this.title = post.title;
+            this.body = post.body;
+            this.slug = post.slug;
+            this.seoKeywords = post.seoKeywords;
+        }
+
+        printPost(){
+            console.log(this.title);
+            console.log(this.body);
+        }
+    }
+}
+
+
+var blogPost = new Blog.Post({
+    title:"My Title",
+    body:"Stuff"
+});
+
+blogPost.printPost();
+
+var contentPost = new Content.Post({
+    title:"My Title",
+    body:"Stuff",
+    slug: "Slug",
+    seoKeywords: "seo"
+});
+
+contentPost.printPost();
+
+
+
+class Invoice {
+    total:number;
+
+
+    constructor(total:number){
+        this.total = total;
+    }
+
+    printTotal(){
+        console.log(this.total);
+    }
+
+    printLater(time:number){
+        setTimeout(function(){
+            console.log(this.total);
+        }, time)
+    }
+
+//by using the arrow function, "this" will reference the class properly, instead of referencing the callback function that it's nexted inside
+//Do this instead!!!!
+    printLaterTwo(time:number){
+        setTimeout(() => {
+            console.log(this.total);
+        }, time)
+    }
+}
+
+var invoice = new Invoice(400);
+
+//results in undefined! "this" is nested inside multiple functions
+//"this" can only be used one layer deep inside a function
+// invoice.printLater(3000);
+
+
+
+invoice.printLaterTwo(3000);
+
+
+//Callback Function
+//a function that is passed inside of another function
+var dbQuery = function() : void {
+    setTimeout(() => {
+        console.log("Query!");
+    }, 3000)
+}
+
+//method that takes a callback function as an arg
+//Higher Order Function
+function loadPage(q: () => void){
+    console.log("Header loaded");
+    q();
+    console.log("Sidebar loaded");
+    console.log("Footer loaded");
+    //Asynchronous stuff!!!
+}
+
+loadPage(dbQuery);
+
+
+//****Typescript has a non-blocking nature******
+
+
+"use strict";
+
+//Promise statuses: Pending, Resolved, Rejected
+
+// let performUpload = function(imgStatus:string):Promise<{imgStatus:string}> {
+//     return new Promise((resolve) => {
+//         console.log(`Status: ${imgStatus}`);
+//         setTimeout(() => {
+//             resolve({imgStatus:imgStatus});
+//         }, 1000);
+//     });
+// }
+//
+// var upload;
+// var compress;
+// var transfer;
+//
+// performUpload("Uploading...")
+//     .then((res) => {
+//         upload = res;
+//         return performUpload("Compressing...");
+//     })
+//     .then((res) => {
+//         compress = res;
+//         return performUpload("Transferring...");
+//     })
+//     .then((res) => {
+//         transfer = res;
+//         return performUpload("Image upload completed.");
+//     });
+
+
+// class Post {
+//     @processOne()
+//     @processTwo()
+//     //someFunction has been decorated
+//     // someFunction(){}
+// }
+
+//decorator must return a function
+function processOne(){
+    console.log("Process One has run.");
+    //typescript compiler requires these args
+    return function(target, propertyKey:string, descriptor:PropertyDescriptor){
+        console.log("Process One has been called.")
+    }
+}
+
+function processTwo(){
+    console.log("Process Two has run.");
+    //typescript compiler requires these args
+    return function(target, propertyKey:string, descriptor:PropertyDescriptor){
+        console.log("Process Two has been called.")
+    }
+}
+
+// Output:
+// Process One has run.
+// Process Two has run.
+// Process Two has been called.
+// Process One has been called.
+
+
+
+
+//Decorators are called at runtime, NOT at object instantiation
+//Notice that Post is never instantiated!
+//Decorators are popular when building permissions structures
+//Decorators help write cleaner code
+
+//
+// @detailedLog("billing")
+// class AccountsPayable {
+//     constructor(){}
+// }
+//
+// @detailedLog("warehouse")
+// class ProductManager {
+//     constructor(){}
+// }
+//
+// //can be layered on top of any class that we want to add a logging functionality to
+// function detailedLog(dashboard:string){
+//     if(dashboard == "billing"){
+//         console.log("Working in billing.");
+//         return function(target:Object){};
+//     }else{
+//         return function(target:Object){};
+//     }
+// }
+//
+// //class decorator needs target
+//
+// var post = new AccountsPayable;
+// var pm = new ProductManager;
+
+//class decorators allowed for shared behavior w/o inheritance
+
+// @detailedLog("billing")
+// class AccountsPayable {
+//     constructor(){}
+//
+//     @admin
+//     deleteAccount(){
+//         console.log("Deleting account.");
+//     }
+// }
+
+function detailedLog(dashboard:string){
+    if(dashboard == "billing"){
+        console.log("Working in billing.");
+        return function(target:Object){};
+    }else{
+        return function(target:Object){};
+    }
+}
+
+function admin(target:Object, propertyKey:string, descriptor:TypedPropertyDescriptor<any>):any{
+    console.log("Doing admin check");
+    return descriptor;
+}
+//
+// var post = new AccountsPayable;
+// post.deleteAccount();
+
+// Output:
+// Doing admin check
+// Working in billing.
+// Deleting account.
+
+
+
+
+type EmployeeType = {
+    email:string;
+    roles?:string[];
+}
+
+let data: {employees?: EmployeeType[]};
+
+data = {
+    employees: [
+        {email: "test@example.com", roles: ["admin", "superAdmin"]},
+        {email: "test2@example.com"},
+        {email: "test3@example.com"}
+    ]
+}
+
+//Done without optional chaining!
+// if (data && data.employees){
+//     formattedEmployees = data.employees.map((employee:EmployeeType) => {
+//         let roles;
+//         if (employee.roles){
+//             roles = employee.roles.join(", ");
+//         }
+//         return `${employee.email.toUpperCase()} ${roles}`;
+//     })
+// }
+
+//Done with optional chaining, much more concise!!
+//Using the "?" allows us to check if something exists
+//Can help prevent errors, esp with API calls
+//Will check first if data returns anything, then checks to see if employees exists, etc.
+const formattedEmployees = data?.employees?.map((employee:EmployeeType) => {
+    const roles = employee.roles?.join(", ");
+    return `${employee.email.toUpperCase()} ${roles}`;
+})
+
+// Output:
+// ["TEST@EXAMPLE.COM admin, superAdmin", "TEST2@EXAMPLE.COM undefined", "TEST3@EXAMPLE.COM undefined"]
+
+
+
+
+
+
+
+
+
+
+console.log(formattedEmployees);
+
+
+
+
+
+
+
+
+
+
 
 
 
